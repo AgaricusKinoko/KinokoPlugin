b043d99d29467c2096504389d1aa3e8efb8ea4fa
Agaricus_Mushroom <beki1240269@gn.iwasaki.ac.jp>
2016-06-04 00:25:53 +0900

    Update damageAbsorb.js

--- damageAbsorb.js
+++ damageAbsorb.js
@@ -1,4 +1,4 @@
-﻿//=============================================================================
+//=============================================================================
 // damageAbsorb.js
 //=============================================================================
 
@@ -13,8 +13,15 @@
 var Kinoko_executeDamage = Game_Action.prototype.executeDamage;	//元々のメソッドを待避
 
 Game_Action.prototype.executeDamage = function(target, value) {
-    Kinoko_executeDamage.call(this,target, value);	//元々のメソッドを呼び出す
+    
     var a = this.subject();	//攻撃者を取得
+    var mpValue = 0;	//吸収する割合の初期値
+    a.states().forEach(function(state){	//攻撃者の全ステートをチェック
+        var amp = state.meta.absorb_MP || 0;	//メモから値を取得。無ければ0とする
+        mpValue += amp;
+    });
+    a.gainMp(parseInt(value * mpValue / 100));	//吸収
+    Kinoko_executeDamage.call(this,target, value);	//元々のメソッドを呼び出す
 };
 
 })();
